2e80aca872eb87289bcddd5bce092b9b8421aeb9
NicolasBello1999 <124221224+NicolasBello1999@users.noreply.github.com>
2023-06-26 02:12:05 -0400

    updates to infixtopostfix

--- infixToPostfix.c
+++ infixToPostfix.c
@@ -5,15 +5,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 #include "stacks.h"
 
-int precedenceCheck(int operator);
+int precedenceCheck(char operator);
+void analyzeString(node **stack, char *input);
+void analyzeStack(node **stack, char symbol);
 
 int main(int argc, char const *argv[])
 {
     node *stack = NULL;
-    int popped;
-
+    int popped, symbol;
+    char input[1000];
+    
     push(&stack, 34);
     push(&stack, 57);
     push(&stack, 81);
@@ -24,10 +30,13 @@ int main(int argc, char const *argv[])
 
     printStack(&stack);
 
-    int symbol;
+    printf("Enter an equation: ");
+    fgets(input, 1000, stdin);
+
+    analyzeString(&stack, input);
 
     // checking for highest priority
-    if (precedenceCheck(peek(&stack)) >= symbol && (symbol != ')' || symbol != '(')) {
+    /*if (precedenceCheck(peek(&stack)) >= symbol && (symbol != ')' || symbol != '(')) {
         printf("%c ", pop(&stack));
         push(&stack, symbol);
     } else if (symbol == ')') {
@@ -36,7 +45,7 @@ int main(int argc, char const *argv[])
             printf("%c ", popped);
         } while (popped != '(');
     }
-    push(&stack, symbol);
+    push(&stack, symbol);*/
 
     return 0;
 }
@@ -50,9 +59,63 @@ int main(int argc, char const *argv[])
     if (peek(&stack))
 }*/
 
-int precedenceCheck(int operator) {
+int precedenceCheck(char operator) {
     if (operator == '*' || operator == '/' || operator == '%')
         return 2;
     else // additive symbols
         return 1;
+}
+
+int isValidOperator(char symbol) {
+    if (symbol == '*' || symbol == '/' || symbol == '+' || symbol == '-' || symbol == '(' || symbol == ')')
+        return 1;
+    else
+        return 0;
+}
+
+void analyzeString(node **stack, char *input) {
+    int i = 0;
+    char buffer[10];
+    node *temp = *stack;
+
+    if (input == NULL) {
+        printf("Input string cannot be NULL.\n");
+        return;
+    }
+
+    printf("Infix to Postfix Expression: \n");
+    while (input[i] != '\n') {
+        strcpy(buffer, "");
+        if (isdigit(input[i]) != 0) {
+            while (isdigit(input[i]) != 0) {
+                strncat(buffer, &input[i], 1);
+                i++;
+            }
+            printf("%s ", buffer);
+        }
+
+        if (isValidOperator(input[i]) == 1)
+            analyzeStack(stack, input[i]);
+
+        i++;
+    }
+    printf("\n");
+}
+
+void analyzeStack(node **stack, char symbol) {
+    int popped;
+    node *temp = *stack;
+
+    if (temp == NULL) {
+        push(&temp, symbol);
+    } else if (precedenceCheck(peek(&temp)) >= symbol && (symbol != ')' || symbol != '(')) {
+        printf("%c ", pop(&temp));
+        push(&temp, symbol);
+    } else if (symbol == ')') {
+        do {
+            popped = pop(&temp);
+            printf("%c ", popped);
+        } while (popped != '(');
+    }
+    //push(&temp, symbol);
 }
