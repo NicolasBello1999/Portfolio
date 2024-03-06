fff93fcc7fa3e05061f59dc324166fc56dfb066c
NicolasBello1999 <124221224+NicolasBello1999@users.noreply.github.com>
2024-03-06 13:01:10 -0500

    Update
    Edited sorting scripts and added to main method/function.

--- Sorting-Algorithms/sorting.cpp
+++ Sorting-Algorithms/sorting.cpp
@@ -1,18 +1,46 @@
 #include <iostream>
+#include <cstdlib>
 #include <string>
+#include <chrono>
+
 using namespace std;
 
 // prototypes
 void swap(int *x, int *y);
-int *bubbleSort(int arr[], int n);
-void insertionSort(int arr[], int n);
-void selectionSort(int arr[], int n);
+int *bubbleSort(int arr[], int size);
+int *insertionSort(int arr[], int size);
+int *selectionSort(int arr[], int size);
 
-void bubbleSort(string arr[], int n);
-void insertionSort(string arr[], int n);
-void selectionSort(string arr[], int n);
+void bubbleSort(string arr[], int size);
+void insertionSort(string arr[], int size);
+void selectionSort(string arr[], int size);
+
+int *fillArrRand(int arr[], int size);
 
 int main(int argc, char *argv) {
+    int **arr, max, i;
+    bool m_flag = false;
+    extern char *optarg; // used for getopt() to return the option argument value
+    extern char optind;  // used by getopt() to communicate how far it scanned the command line
+    char opt;
+
+    while ((opt = getopt(argc, argv, ":m")) != -1) {
+        switch (opt) {
+            case 'm':
+                m_flag = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (!m_flag)
+        max = 100000;
+
+    arr = new int[4][max];
+    for (i = 0; i < 4; i++) {
+        arr[i] = fillArrRand(arr[i], max);
+    }
 
     return 0;
 }
@@ -25,13 +53,13 @@ void swap(int *x, int *y) {
     *y = temp;
 }
 
-int *bubbleSort(int arr[], int n) {
-    int x, y, temp;
+int *bubbleSort(int arr[], int size) {
+    int i, j, temp;
 
-    for (x = 0; x < n - 1; x++) {
-        for (y = 0; y < n - 1 - x; y++) {
-            if (arr[x] > arr[x + 1]) {
-                swap(arr[x], arr[x + 1]);
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - x; j++) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
             }
         }
     }
@@ -39,10 +67,44 @@ int *bubbleSort(int arr[], int n) {
     return arr;
 }
 
-void insertionSort(int arr[], int n) {
+int *insertionSort(int arr[], int size) {
+    int i, j, key;
 
+    for (i = 0; i < size; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    return arr;
 }
 
-void selectionSort(int arr[], int n) {
+int *selectionSort(int arr[], int size) {
+    int i, j, min_idx;
 
+    for (i = 0; i < size - 1; i++) {
+        min_idx = i;
+
+        for (j = i + 1; j < size; j++) {
+            if (arr[j] <  arr[min_idx])
+                min_idx = j;
+            swap(arr[i], arr[min_idx]);
+        }
+    }
+
+    return arr;
+}
+
+int *fillArrRand(int arr[], int size) {
+    int i;
+    
+    srand((unsigned) time(NULL));
+    for (i = 0; i < size; i++)
+        arr[i] = (rand() % max) + 1;
+    
+    return arr;
 }
